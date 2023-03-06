3f48515d62e804b16284777399da6e50c3e6a8d5
tareklinkup <tarek@linktechbd.com>
2023-03-06 15:30:06 +0600

    first working in this project

---
+++ application/config/database.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$active_group = 'default';
+$query_builder = TRUE;
+
+$db['default'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	
+  	'username' => 'dewbxcak_admin5',
+  	'password' => '(h-BfA?74WM)Z',
+  	'database' => 'dewbxcak_helixelectricals',
+	
+// 	'username' => 'root',
+// 	'password' => '',
+// 	'database' => 'production',
+		
+
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
